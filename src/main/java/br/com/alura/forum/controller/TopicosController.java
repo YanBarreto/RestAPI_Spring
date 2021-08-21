785ce9f75c69c5d3aa9b1483bce7e27a124bcac4
YanBarreto <40630801+YanBarreto@users.noreply.github.com>
2021-08-21 12:53:57 -0300

    HOTFIX - TopicoForm nao convertia as informacoes

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.TopicosRepository;
 
--- src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -1,27 +1,19 @@
 package br.com.alura.forum.controller.form;
 
+import br.com.alura.forum.controller.CursoRepository;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
 public class TopicoForm {
 	
 	private String titulo;
-    private String mensagem;
-    private String nomeCurso;
-    
-    
-    
-	public String getTitulo() {
+	private String nomeCurso;
+	private String mensagem;
+	public String getNome() {
 		return titulo;
 	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getMensagem() {
-		return mensagem;
-	}
-	public void setMensagem(String mensagem) {
-		this.mensagem = mensagem;
+	public void setNome(String nome) {
+		this.titulo = nome;
 	}
 	public String getNomeCurso() {
 		return nomeCurso;
@@ -29,11 +21,18 @@ public class TopicoForm {
 	public void setNomeCurso(String nomeCurso) {
 		this.nomeCurso = nomeCurso;
 	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
 	
-	public Topico converter(Curso curso, TopicoForm topicoForm) {
-		Topico topico = new Topico(topicoForm.getTitulo(), topicoForm.getMensagem(), curso);
-		return topico;
+	public Topico converter(CursoRepository cursoRepository) {
+		
+		Curso curso = cursoRepository.findByNome(nomeCurso);		
+		return new Topico(titulo, mensagem, curso);
 	}
 
-
+	
 }
