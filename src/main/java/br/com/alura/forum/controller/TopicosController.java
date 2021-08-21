ba3358e2c07ff0d967fd55a98ecfffdf6eca06ce
YanBarreto <40630801+YanBarreto@users.noreply.github.com>
2021-08-21 12:44:24 -0300

    adicao do response entity para retorno 201 created

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,12 +1,16 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Topico;
@@ -39,10 +43,12 @@ public class TopicosController {
 	}
 	
 	@PostMapping
-	public void cadastrar(@RequestBody TopicoForm form){
-		
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder){
 		Topico topico = form.converter(cursosRepository);
 		topicosRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri(); 
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
 	
 	
